bfd17d26c6040deeff1cffacd0df6eb4b463dbed
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-19 05:04:51 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    @@songs.each do |song|
+      resp.write "#{song.title}\n"
+    end
+ 
+    resp.finish
+  end
+end
